2f30ab7c34d40438ce608e8c86639be428e299b9
tanXnyx <129646121+tanXnyx@users.noreply.github.com>
2023-08-07 23:54:09 +0530

    string builder

---
+++ stringbuilder.java
@@ -0,0 +1,9 @@
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("");
+        for (char ch='a';ch<='z';ch++){
+              sb.append(ch);
+        }
+        System.out.println("StringBuilder:"+sb+" length="+sb.length());
+    }
+}
