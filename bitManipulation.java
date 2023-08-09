64b22f4ef0698a5e4e4ebc05e93ecda13762cf18
tanXnyx <129646121+tanXnyx@users.noreply.github.com>
2023-08-09 22:34:16 +0530

    bit Manipulation

---
+++ bitManipulation.java
@@ -0,0 +1,12 @@
+public class bitManipulation {
+    public static void main(String[] args) {
+        System.out.println((5 & 6));
+        System.out.println((5 | 6));
+        System.out.println((5 ^ 6));
+        System.out.println((~5));
+        // a<<b = a*(2.math.power(b))
+        System.out.println((5<<2));
+        //a>>b = a/(2.math.power(b))
+         System.out.println((6>>1));
+    }
+}
