d1099fb434ce6d46b0e4ec9d07e1e18ee2beb6fc
tanXnyx <129646121+tanXnyx@users.noreply.github.com>
2023-08-18 21:45:22 +0530

    nested Try Catch

---
+++ nestedTryCatch.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class nestedTryCatch {
+    public static void main(String[] args) {
+        int [] marks = new int[3];
+        marks[0] = 7;
+        marks[1] = 56;
+        marks[2] = 6;
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        while(flag) {
+            System.out.println("Enter the value of index");
+            int ind = sc.nextInt();
+            try {
+                System.out.println("Welcome");
+                try {
+                    System.out.println(marks[ind]);
+                    flag = false;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Sorry this index does not exist");
+                    System.out.println("Exception in level 2");
+                }
+            } catch (Exception e) {
+                System.out.println("Exception in level 1");
+            }
+        }
+        System.out.println("Thanks for using this program");
+    }
+}
+/*
+ * The Exception class has the following important methods:
+
+1) Strings toString() executed when sout (e) is ran
+
+2) Void printStackTrace() - prints Stack trace
+
+3) String getMessage() - prints the exception message
+ */
