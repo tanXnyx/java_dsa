5199df294f9a04c7f19b97c1c64f18f0b9f554a2
tanXnyx <129646121+tanXnyx@users.noreply.github.com>
2023-08-18 21:44:31 +0530

    Throw Vs Throws.

---
+++ ThrowVsThrows.java
@@ -0,0 +1,39 @@
+class NegativeRadiusException extends Exception{
+    @Override
+    public String toString() {
+        return "Radius cannot be negative!";
+    }
+
+    @Override
+    public String getMessage() {
+        return "Radius cannot be negative!";
+    }
+}
+public class ThrowVsThrows {
+    public static double area(int r) throws NegativeRadiusException{
+        if (r<0){
+            throw new NegativeRadiusException();
+        }
+        double result = Math.PI * r * r;
+        return result;
+    }
+
+    public static int divide(int a, int b) throws ArithmeticException{
+        
+        int result = a/b;
+        return result;
+    }
+    public static void main(String[] args) {
+        
+        try{
+//            int c = divide(6, 0);
+//            System.out.println(c);
+            double ar = area(-1);
+            System.out.println(ar);
+        }
+        catch(Exception e){
+            System.out.println("Exception");
+        }
+    }
+
+}
