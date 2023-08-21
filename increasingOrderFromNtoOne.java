0c5da56ee29c28b98a14523a7a2dc5c65f262a2c
tanXnyx <129646121+tanXnyx@users.noreply.github.com>
2023-08-21 22:11:13 +0530

    increasing Order From N to One

---
+++ increasingOrderFromNtoOne.java
@@ -0,0 +1,14 @@
+public class increasingOrderFromNtoOne {
+    public static void printInc(int n){
+        if(n==1){
+            System.out.println(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.println(n+"");
+    }
+    public static void main(String[] args) {
+        int n =10;
+        printInc(n);
+    }
+}
