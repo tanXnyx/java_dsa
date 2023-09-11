3929f4e7369fe69fc5bbd901a46ba8aa7514a05c
tanmay-Kumar-Swain <129646121+tanXnyx@users.noreply.github.com>
2023-09-11 20:51:45 +0530

    Add First LL

---
+++ AddFirstLL.java
@@ -0,0 +1,35 @@
+public class AddFirstLL {
+    class Node{
+        int data;
+        Node next;
+        public Node (int data){
+            this.data =data ;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    //adding node at first (before head)
+    public void addFirst(int data){
+        //Step1 = create new node 
+        Node newNode = new Node(data);
+        size++;
+        //Check if lickedlist is empty
+        if(head == null ){
+            head = tail= newNode;//if linkedlist is empty then head and tail is same as the new node 
+            return;
+        }
+        //Step2 = newNode next = head
+        newNode.next = head;
+        //Step3 = head = nowNode
+        head = newNode;
+    }
+    public static void main(String[] args) {
+        AddFirstLL ll = new AddFirstLL();
+        ll.addFirst(2);
+        ll.addFirst(1);
+  }
+
+}
