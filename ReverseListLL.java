151880c89e59f1ad2f3eca1ba3130bc80c1bb40e
tanmay-Kumar-Swain <129646121+tanXnyx@users.noreply.github.com>
2023-09-21 21:57:55 +0530

    Reverse List LL

---
+++ ReverseListLL.java
@@ -0,0 +1,27 @@
+public class ReverseListLL {
+    class Node{
+        int data;
+        Node next;
+        public Node (int data){
+            this.data =data ;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void reverseList(){
+        Node prev= null;
+        Node current = tail = head;
+        Node next;
+        while(current!= null){
+            next = current.next;
+            current.next =prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+ }
+
+}
